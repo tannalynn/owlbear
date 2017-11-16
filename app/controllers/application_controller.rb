415f499d1706181f082005fc2001f01ba2e78bec
tanna <tanna.la.lynn@gmail.com>
2017-11-16 23:44:00 +0000

    Added static pages controller for help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  layout 'application'
+  
+  
+  def login_page
+
+    #render html: "LOGIN PAGE!!"  
+  end
+  
+  
 end
